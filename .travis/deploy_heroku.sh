45a77e25f1da27901f782734aa7fc57b712aca02
Christos Tsotskas <c.tsotskas@gmail.com>
2020-04-19 18:24:39 +0100

    fixing travisCI

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -6,6 +6,3 @@ docker build  -t registry.heroku.com/aristoptimiser/web -f Dockerfile .
 docker push registry.heroku.com/aristoptimiser/web
 heroku container:release web --app=aristoptimiser
 
-heroku ps:scale web=1 -a aristoptimiser
-pipenv run pytest -v test/test_the_deployment_of_containers.py
-heroku ps:scale web=0 -a aristoptimiser
